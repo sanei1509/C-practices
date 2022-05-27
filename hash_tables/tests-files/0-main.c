657fcbe4615a0a45d3bc76b0514e96902abcd498
Santiago Neira <4373@holbertonschool.com>
2022-05-27 19:42:08 +0000

    añadiendo pruebas para comprobar que se haya creado bien

--- hash_tables/tests-files/0-main.c
+++ hash_tables/tests-files/0-main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "hash_tables.h"
+#include "../hash_tables.h"
 
 /**
  * main - check the code for
@@ -13,6 +13,10 @@ int main(void)
     hash_table_t *ht;
 
     ht = hash_table_create(1024);
+    /*valor memoria del hash table*/
     printf("%p\n", (void *)ht);
+    /*comprobamos que se haya creado del tamaño esperado*/
+    printf("%lu\n", ht->size);
+    
     return (EXIT_SUCCESS);
 }
